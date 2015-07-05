1ad648ec784e188668e6685e34ca373f6243f0b0
Giovanni Petrache <giovanni.petrache@gmail.com>
2015-07-06 01:01:51 +0800

    Customizing zfc-user views template. Implementing twb and twig.

--- config/module.config.php
+++ config/module.config.php
@@ -3,6 +3,15 @@
  * var $module and $module_dir is define in Module Class
  */
 return array(
+    'view_manager' => array(
+        'template_map' => array(
+            'user/login/form' => "{$module_dir}/view/zfc-user/user/login/form.twig",
+        ),
+        'template_path_stack' => array(
+            // Override zfcuser view templates
+            'zfcuser' => "{$module_dir}/view",
+        ),
+    ),
     'doctrine' => array(
         'driver' => array(
             'orm_default' => array(
--- src/SpacedGAPRbac/Module.php
+++ src/SpacedGAPRbac/Module.php
@@ -4,18 +4,45 @@ namespace SpacedGAPRbac;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 use SpacedGAP\AbstractZf2Module;
+use Zend\Navigation\Page\Mvc;
 
 class Module 
     extends AbstractZf2Module
 {
-/*
+
     public function onBootstrap(MvcEvent $e)
     {
+        parent::onBootstrap($e);
+
         $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
+        $eventSharedManager  = $eventManager->getSharedManager();
+
+        $eventSharedManager->attach('ZfcUser\Form\Login', 'init', [$this, 'updateZfcUserFormLogin']);
     }
 
+    public function updateZfcUserFormLogin (\Zend\EventManager\Event $e)
+    {
+        $form = $e->getTarget();
+
+        $identity         = $form->get('identity');
+        $identity_options = $identity->getOptions();
+
+        $identity_options['twb-layout']       = \TwbBundle\Form\View\Helper\TwbBundleForm::LAYOUT_HORIZONTAL;
+        $identity_options['column-size']      = 'md-10';
+        $identity_options['label']            = $identity->getLabel();
+        $identity_options['label_attributes'] = array('class' => 'col-md-2');
+        $identity->setOptions($identity_options);
+
+        $credential         = $form->get('credential');
+        $credential_options = $credential->getOptions();
+
+        $credential_options['twb-layout']       = \TwbBundle\Form\View\Helper\TwbBundleForm::LAYOUT_HORIZONTAL;
+        $credential_options['column-size']      = 'md-10';
+        $credential_options['label']            = $credential->getLabel();
+        $credential_options['label_attributes'] = array('class' => 'col-md-2');
+        $credential->setOptions($credential_options);
+    }
+/*
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
